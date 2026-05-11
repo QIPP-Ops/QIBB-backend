2963df6b310ba95e3b3599cf27b3e33bc410ea70
Mohammad AlGarni <m.algarni@nomac.com>
2026-05-11 19:06:00 +0000

    Updated dailyOperationRoutes.js

--- routes/dailyOperationRoutes.js
+++ routes/dailyOperationRoutes.js
@@ -5,5 +5,6 @@ const ctrl    = require('../controllers/dailyOperationController');
 router.get('/',         ctrl.getLatest);
 router.get('/bydate',   ctrl.getByDate);
 router.get('/summary',  ctrl.getSummary);
+router.get('/kpis',     ctrl.getKpis);
 
 module.exports = router;
