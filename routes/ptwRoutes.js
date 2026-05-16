d1cab9d0e9319e8fd8a584b3b1a9e160c7689d8a
QIPP-Ops <m.algarni@nomac.com>
2026-05-16 09:56:49 +0300

    Update ptwRoutes.js

--- routes/ptwRoutes.js
+++ routes/ptwRoutes.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const ptwController = require('../controllers/ptwController');
 const { protect } = require('../middleware/auth');
 
+// Permits routes (both short and /permits paths)
+router.get('/',                  ptwController.getAllPermits);
+router.get('/permits',           ptwController.getAllPermits);
 
-router.get('/', ptwController.getAllPermits);
+router.post('/',                 protect, ptwController.createPermit);
+router.post('/permits',          protect, ptwController.createPermit);
 
-// Mutations require protection
-router.post('/', protect, ptwController.createPermit);
-router.patch('/:id', protect, ptwController.updatePermitStatus);
-router.delete('/:id', protect, ptwController.deletePermit);
+router.put('/:id',               protect, ptwController.updatePermitStatus);
+router.patch('/:id',             protect, ptwController.updatePermitStatus);
+router.put('/permits/:id',       protect, ptwController.updatePermitStatus);
+router.patch('/permits/:id',     protect, ptwController.updatePermitStatus);
+
+router.delete('/:id',            protect, ptwController.deletePermit);
+router.delete('/permits/:id',    protect, ptwController.deletePermit);
 
 module.exports = router;
